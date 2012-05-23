fc25710b1f1a5551c0e9779fae19907f7bf55f37
Lars Schwegmann <larsob@me.com>
2012-05-23 20:04:33 +0200

    Tweaked design
    tweaked the design a little to fit the colorscheme of humanstxt.org

--- humanstxt/humanstxt/ViewController.m
+++ humanstxt/humanstxt/ViewController.m
@@ -145,7 +145,7 @@
     
     headerView = [[UIView alloc] init];
     background = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 22)];
-    background.image = [UIImage imageNamed:@"tableheader"];
+    background.image = [UIImage imageNamed:@"tableheader3"];
     
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(5, 5, 200, 10)];
     label.backgroundColor = [UIColor clearColor];
@@ -160,7 +160,7 @@
      label.textColor = [UIColor redColor];
      }
      label.shadowColor = [UIColor colorWithWhite:0.0 alpha:0.5];*/
-    label.textColor = [UIColor grayColor];
+    label.textColor = [UIColor whiteColor];
     
     //status = [[UIView alloc] initWithFrame:CGRectMake(265, 0, 70, 22)];
     //status.layer.cornerRadius = 10.0f;
@@ -192,10 +192,25 @@
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     static NSString *CellIdentifier = @"Cell";
     
+    UIView *theContentView;
+    UIImageView *backgroundView;
+    
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        theContentView = [[UIView alloc] init];
+        theContentView.tag = 0;
+        backgroundView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 40)];
+        backgroundView.image = [UIImage imageNamed:@"cell"];
+        backgroundView.tag = 1;
+        [theContentView addSubview:backgroundView];
+        [cell.contentView addSubview:theContentView];
+    }else{
+        theContentView = (UIView *)[cell.contentView viewWithTag:0];
+        backgroundView = (UIImageView *)[[cell.contentView viewWithTag:0] viewWithTag:1];
     }
+    
+    
     
     // Configure the cell...
     //NSString *continent = [self tableView:tableView titleForHeaderInSection:indexPath.section];
@@ -204,18 +219,14 @@
     for (int i = indexPath.section-1; i>=0; i--) {
         minusRows = minusRows + [self tableView:theTableView numberOfRowsInSection:i];
     }
+    
     NSDictionary *dict = [theHumanTXTObjects objectAtIndex:indexPath.row+minusRows];
     NSLog(@"original indexpath.section: %d", indexPath.section);
     NSLog(@"modified version of indexPath.section: %d", indexPath.section+1);
     NSLog(@"section of content dictionary: %d", [[dict objectForKey:@"section"] intValue]);
-    if ([[dict objectForKey:@"heading"] isEqualToString:@"false"]) {
-        /*if (indexPath.section+1 == [[dict objectForKey:@"section"] intValue]) {
-            [cell.textLabel setText:[dict objectForKey:@"content"]];
-        }*/
-        [cell.textLabel setText:[dict objectForKey:@"content"]];
-    }
     
-    
+    [cell.textLabel setText:[dict objectForKey:@"content"]];
+    [cell.textLabel setBackgroundColor:[UIColor clearColor]];
     cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     return cell;
 }
